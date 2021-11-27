3bb690f328335b1c1d6ce7ee9a52bd5c0301853f
Jianzhi Wang <jianzhi@berkeley.edu>
2021-11-26 23:12:13 -0800

    fixed some logic

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Darboux Parser
+Created for CS61A A+ Program
+[Link to YouTube](www.youtube.com)
 
+### Idea
+A simple BNF parser that is created by higher order functions.
+
+### Algorithms
+Darboux Parser features two main algorithms:
+1. ```matcher```
+2. ```interval_matcher```
+
+### Extension
+Interval matching - matching of strings first
+
+### Set Up
+```shell
+$ python3 parser.py
+```
 
 ### Remark
 The name Darboux was chosen because the author was studying partitions under the topic of Darboux integral in MATH 104.
--- parser.py
+++ parser.py
@@ -8,6 +8,45 @@ import re
 
 ### stack can consist of characters, functions (every symbol is a function)
 
+test = '"(" correct ")" correct'
+# stack ['"("', 'correct', '")"', 'correct']
+# stack ['"("', hof, '")"', hof]
+
+
+# match string with stack
+def matcher(s, st):
+    if len(st) == 0 and s == "":
+        return True
+    if len(st) == 0:
+        return False
+    if type(st[0]) is str:
+        if not s.startswith(st[0]):
+            return False
+        return matcher(s[len(st[0]):], st[1:])
+    return any([(st[0](s[:i]) and matcher(s[i:], st[1:])) for i in range(len(s))])
+
+# DarbouxParser class
+class DarbouxParser:
+
+    def __init__(self):
+        # maps name to dict
+        self.hof_dict = dict()
+    
+    # Takes in a string and see if it matches
+    def match(self, hof_key, string):
+        return self.hof_dict[hof_key](string)
+    
+    # Takes in a list and construct the hof function
+    def construct(self, hof_key, lst):
+        for i in range(len(lst)):
+            if len(lst[i]) > 0 and lst[i][0] == '"':
+                continue
+            if lst[i] not in self.hof_dict:
+                self.hof_dict[lst[i]] = lambda x: False
+            else:
+                lst[i] = self.hof_dict[lst[i]]
+        self.hof_dict[hof_key] = lambda x: matcher(x, lst)
+
 def split(string):
     # split string based on symbols, chars
     return []
